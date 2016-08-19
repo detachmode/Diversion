5ae9c6cc7c9a0861320df38d49ca75e97aa77c85
Dennis <Dennis@PC308>
2016-08-19 17:10:48 +0200

    Projektdateien hinzufügen.

---
+++ Diversion/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diversion
+{
+
+    public class Diversion
+    {
+        public List<string> permutations;
+
+        public Diversion()
+        {
+            permutations = new List<string>();
+        }
+        public int find_matching_combinations(int bitcount)
+        {
+            createPermutations(bitcount);
+            filteroutNotMatchingElements();
+
+            return permutations.Count;
+        }
+
+        public void filteroutNotMatchingElements()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void createPermutations(int bitcount)
+        {
+            createPermutationsRecursive(bitcount, "");
+        }
+
+
+        public void createPermutationsRecursive(int bitcount, string str)
+        {
+            appendBitRecursiveAndFinallyAddToPermutations(str, "0", bitcount);
+            appendBitRecursiveAndFinallyAddToPermutations(str, "1", bitcount);
+        }
+
+
+        public event Action<string, int> addToCombination;
+
+
+        public void appendBitRecursiveAndFinallyAddToPermutations(string str, string thechar, int bitcount)
+        {
+            if (bitcount == 0)
+            {
+                permutations.Add(str);
+                return;
+            }   
+                   
+            str += thechar;
+            createPermutationsRecursive(--bitcount, str);
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ DiversionTests/DiversionTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Diversion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Diversion.Tests
+{
+    [TestClass()]
+    public class DiversionTests
+    {
+        [TestMethod()]
+        public void append_0_recursionTest()
+        {
+            //var div = new Diversion();
+            //div.append_0_recursion("", 1);
+            //Assert.AreEqual(1, div.permutations.Count);
+
+        }
+
+        [TestMethod()]
+        public void buildAllPossiblePermutationsTest()
+        {
+            var div = new Diversion();
+            div.createPermutations(2);
+
+            Assert.AreEqual(4, div.permutations.Count);
+            Assert.AreEqual(true, div.permutations.Find(x => x == "00") != null);
+            Assert.AreEqual(true, div.permutations.Find(x => x == "01") != null);
+            Assert.AreEqual(true, div.permutations.Find(x => x == "11") != null);
+
+            div = new Diversion();
+            div.createPermutations(4);
+            Assert.AreEqual(true, div.permutations.Find(x => x == "0001") != null);
+
+        }
+
+        [TestMethod()]
+        public void appendBitRecursiveAndFinallyAddToPermutationsTest()
+        {
+            //var div = new Diversion();
+            //div.appendBitRecursiveAndFinallyAddToPermutations("","1",3);
+
+            //Assert.AreEqual("11", div.permutations[1]);
+        }
+    }
+}
