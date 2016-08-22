064006015a77bafe1ce6d35b6ccc809437bfd743
Dennis <dennis.briefkasten@gmail.com>
2016-08-22 16:07:03 +0200

    modfiyList function added to make it more declarative

--- Diversion/Program.cs
+++ Diversion/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,7 +14,7 @@ namespace Diversion
     {
         public List<string> permutations;
         private readonly int bitcount;
-        List<string> workingList;
+
 
         public Diversion(int bitcount)
         {
@@ -24,16 +27,48 @@ namespace Diversion
         //event Action<string> 
 
 
-        public List<string> CreatePermutationIterative(int size, Action<List<string>> buildPermutationFunc)
+        public List<string> CreatePermutationIterative(int size)
         {
+            List<string> workingList;
+
             workingList = CreateInitialList();
 
-            for (int i = 0; i < size - 1; i++)
-            {
-                workingList = AddNextBiggerPermutations(workingList);
-            }
+            var liste = ModifyList(workingList, iterations: 3, newList: x => x.Select(AddAusrufezeichen).ToList());
+
+            workingList = ModifyList(workingList, iterations: size-1, newList: NextBiggerPermutations);
 
             return workingList;
+        }
+        
+        private string AddAusrufezeichen(string y)
+        {
+            string str = "_";
+            for (int i = 1; i <= y.Length; i++)
+            {
+                str += i.ToString();
+            }
+            str += "_";
+
+            return y + str;
+        }
+
+        private List<string> ModifyList(List<string> theList, int iterations, Func<List<string>, List<string>> newList)
+        {
+            for (int i = 0; i < iterations ; i++)
+            {
+                theList = newList(theList);
+            }
+            return theList;
+        }
+
+        public List<string> NextBiggerPermutations(List<string> workingList)
+        {
+            var list1 = workingList.Select(x => x + "0").ToList();
+            var list2 = workingList.Select(x => x + "1").ToList();
+
+            list1.AddRange(list2);
+
+            return list1;
         }
 
         private static List<string> CreateInitialList()
@@ -42,25 +77,15 @@ namespace Diversion
             return workingList;
         }
 
-        public List<string> AddNextBiggerPermutations(List<string> workingList)
-        {
-            var list1 = AddCharToAllStrings(workingList, '0');
-            var list2 = AddCharToAllStrings(workingList, '1');
-
-            list1.AddRange(list2);
-
-            return list1;
-        }
-
-        private static List<string> AddCharToAllStrings(List<string> workingList, char c)
-        {
-            var list1 = new List<string>();
-            foreach (string s in workingList)
-            {
-                list1.Add(s + c);
-            }
-            return list1;
-        }
+        //private static List<string> AddCharToAllStrings(List<string> workingList, char c)
+        //{
+        //    var list1 = new List<string>();
+        //    foreach (string s in workingList)
+        //    {
+        //        list1.Add(s + c);
+        //    }
+        //    return list1;
+        //}
 
 
         private void PermutationConsumer(string str, int stillMissing)
